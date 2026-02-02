95c04c0aa6ab169e57dfee8cd672147865b5db34
Bakpo Godwin <67370368+Hood-Codivo@users.noreply.github.com>
2026-02-02 18:35:25 +0100

    NFTs task completed

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,11 +1,16 @@
-import wallet from "../turbin3-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
-import { readFile } from "fs/promises"
+import wallet from "./wallet/turbin3-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFile } from "fs/promises";
+import path from "path";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
@@ -14,17 +19,25 @@ umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        //1. Load image
-        //2. Convert image to generic file.
-        //3. Upload image
+  try {
+    //1. Load image
+    const buffer = await readFile(
+      path.join(__dirname, "assets", "privyNote.png"),
+    );
 
-        // const image = ???
+    //2. Convert image to generic file.
+    const file = createGenericFile(buffer, "privyNote.png", {
+      contentType: "image/png",
+    });
 
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    //3. Upload image
+    const [imageUri] = await umi.uploader.upload([file]);
+
+    console.log("Your image URI: ", imageUri);
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
+
+// Your image URI:
+// https://gateway.irys.xyz/HgWVB6oy3Ft6sihvHpV3VQjJSEfph6EZkAmzxny4RPc3
